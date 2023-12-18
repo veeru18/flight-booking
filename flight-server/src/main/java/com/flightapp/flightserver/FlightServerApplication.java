38ea1414959fc60885cf2996b4b82ac7602fc426
Veeresh T A <veereshta@gmail.com>
2023-12-18 23:38:58 +0530

    adding flight-test app

---
+++ flight-server/src/main/java/com/flightapp/flightserver/FlightServerApplication.java
@@ -0,0 +1,13 @@
+package com.flightapp.flightserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightServerApplication.class, args);
+	}
+
+}
---
+++ flight-server/src/test/java/com/flightapp/flightserver/FlightServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flightapp.flightserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
