bd39238ba5480d7aa72dbcfa4d13bd8d0ed0a93c
Veeresh T A <veereshta@gmail.com>
2024-01-15 18:13:17 +0530

    using h2dB

---
+++ flight-server/src/main/java/com/flightapp/flightserver/model/Task.java
@@ -0,0 +1,13 @@
+package com.flightapp.flightserver.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class Task {
+    private String taskName;
+}
---
+++ flight-server/src/main/java/com/flightapp/flightserver/repository/TaskRepository.java
@@ -0,0 +1,4 @@
+package com.flightapp.flightserver.repository;
+
+public interface TaskRepository {
+}
